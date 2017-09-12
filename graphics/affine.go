9176ab3e3475a576e630789ca9838e9c03e1265c
sed <bsed@users.noreply.github.com>
2017-09-12 15:53:32 +0800

    Update affine.go

--- graphics/affine.go
+++ graphics/affine.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package graphicsbse
+package graphics
 
 import ( 
 	"github.com/bsed/graphics-go/graphics/interp"
